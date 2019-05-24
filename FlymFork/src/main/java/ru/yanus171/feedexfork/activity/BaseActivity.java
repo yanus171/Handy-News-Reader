2bab439f0c7fb716a761b4d52386bc5651954ca3
yanus171 <workyalex@mail.ru>
2019-05-25 00:04:05 +0300

    language select fixes

--- FlymFork/src/main/java/ru/yanus171/feedexfork/activity/BaseActivity.java
+++ FlymFork/src/main/java/ru/yanus171/feedexfork/activity/BaseActivity.java
@@ -18,13 +18,21 @@
  */
 package ru.yanus171.feedexfork.activity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
+import android.os.Build;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+
+import java.util.Locale;
 
 import ru.yanus171.feedexfork.Constants;
 import ru.yanus171.feedexfork.R;
 import ru.yanus171.feedexfork.utils.Brightness;
+import ru.yanus171.feedexfork.utils.PrefUtils;
 import ru.yanus171.feedexfork.utils.UiUtils;
 
 
@@ -35,6 +43,7 @@ public abstract class BaseActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        InitLocale(this);
         UiUtils.setPreferenceTheme(this);
         super.onCreate(savedInstanceState);
         mDecorView = getWindow().getDecorView();
@@ -64,5 +73,35 @@ public abstract class BaseActivity extends AppCompatActivity {
             mBrightness.OnPause();
         }
         super.onPause();
+    }
+    // ----------------------------------------------------------------
+    private final static String defaultLanguage = "System";
+    private static Locale GetLocale(Context context) {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        String lang = prefs.getString(PrefUtils.LANGUAGE, defaultLanguage);
+        if (lang.equals(defaultLanguage)) {
+            if (context.getResources().getConfiguration().locale != null) {
+                lang = context.getResources().getConfiguration().locale.getLanguage();
+            }
+        }
+        return new Locale(lang);
+    }
+
+    public static Context InitLocale( final Context context ) {
+        Context newContext = context;
+        Locale locale = GetLocale(context);
+        Locale.setDefault(locale);
+        Configuration config = new Configuration();
+        config.locale = locale;
+        if (Build.VERSION.SDK_INT >= 17) {
+            newContext = context.createConfigurationContext(config);
+        } else
+            context.getResources().updateConfiguration(config, context.getResources().getDisplayMetrics());
+        return newContext;
+    }
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext( InitLocale( base ));
     }
 }
