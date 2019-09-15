bfc610a4e53be23060b8e3237546e1ec921b197e
yanus171 <workyalex@mail.ru>
2019-09-15 12:55:44 +0300

    AutoSummaryListPreference to kt

---
+++ FlymFork/src/main/java/ru/yanus171/feedexfork/view/StorageSelectPreference.kt
@@ -0,0 +1,8 @@
+package ru.yanus171.feedexfork.view
+
+import android.content.Context
+import android.util.AttributeSet
+
+class StorageSelectPreference (context: Context, attrs: AttributeSet) : android.preference.ListPreference(context, attrs) {
+
+}
