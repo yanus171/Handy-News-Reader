e8609b381cfea786c50b888175840a136731d8fd
SIRSteiner <github@beisteiners.de>
2020-05-17 03:49:23 +0200

    New theme BLACK

--- FlymFork/src/main/java/ru/yanus171/feedexfork/utils/Theme.java
+++ FlymFork/src/main/java/ru/yanus171/feedexfork/utils/Theme.java
@@ -4,8 +4,14 @@ import java.util.HashMap;
 
 import android.app.AlertDialog;
 import android.content.Context;
+import android.content.res.Resources;
 import android.graphics.Color;
+import android.net.MailTo;
 import android.os.Build;
+import android.widget.Toast;
+
+import androidx.core.content.ContextCompat;
+import androidx.core.content.res.ResourcesCompat;
 
 import ru.yanus171.feedexfork.MainApplication;
 import ru.yanus171.feedexfork.R;
@@ -19,6 +25,7 @@ public class Theme {
 	static final String BUTTON_BACKGROUND_RES_ID = "ButtonBackgroundResID";
 	static final String DARK = "Dark";
 	static final String LIGHT = "Light";
+	static final String BLACK = "Black";
 	public static final String TEXT_COLOR_BACKGROUND = "textColor_background";
 	public static final String TEXT_COLOR = "textColor";
 	public static final String TEXT_COLOR_READ = "textColorRead";
@@ -52,8 +59,11 @@ public class Theme {
 
 			{
 				HashMap<String, String> theme = new HashMap<String, String>();
-				theme.put(TEXT_COLOR, "#555555");
-				theme.put(TEXT_COLOR_BACKGROUND, "#f6f6f6");
+//				theme.put(TEXT_COLOR, "#555555");
+				theme.put(TEXT_COLOR, "#"+Integer.toHexString(ContextCompat.getColor(MainApplication.getContext(), R.color.light_theme_color_unread)).substring(2));
+//				theme.put(TEXT_COLOR_BACKGROUND, "#f6f6f6");
+				theme.put(TEXT_COLOR_BACKGROUND, "#"+Integer.toHexString(ContextCompat.getColor(MainApplication.getContext(), R.color.light_theme_background)).substring(2));
+				theme.put(TEXT_COLOR_READ, "#"+Integer.toHexString(ContextCompat.getColor(MainApplication.getContext(), R.color.light_theme_color_read)).substring(2));
 				theme.put(MENU_FONT_COLOR, "#000000");
 				theme.put(MENU_BACKGROUND_COLOR, "#CCCCCC");
 				theme.put(QUOTE_BACKGROUND_COLOR, "#e6e6e6");
@@ -64,7 +74,7 @@ public class Theme {
 
                 //theme.put(ENTRY_LIST_BACKGROUND, "#ffeeeeee");
 				theme.put(STYLE_THEME, String.valueOf( R.style.Theme_Light) );
-
+				theme.put("toolBarColor", "#"+Integer.toHexString(ContextCompat.getColor(MainApplication.getContext(), R.color.light_theme_color_primary)).substring(2));
 
 //				theme.put("themeDialog", String.valueOf( R.style.styleLightDialog) );
 				ThemeList.put(LIGHT, theme);
@@ -72,8 +82,11 @@ public class Theme {
 
 			{
 				HashMap<String, String> theme = new HashMap<String, String>();
-				theme.put(TEXT_COLOR, "#FFFFFF");
-				theme.put(TEXT_COLOR_BACKGROUND, "#181b1f");
+//				theme.put(TEXT_COLOR, "#FFFFFF");
+				theme.put(TEXT_COLOR, "#"+Integer.toHexString(ContextCompat.getColor(MainApplication.getContext(), R.color.dark_theme_color_unread)).substring(2));
+//				theme.put(TEXT_COLOR_BACKGROUND, "#181b1f");
+				theme.put(TEXT_COLOR_BACKGROUND, "#"+Integer.toHexString(ContextCompat.getColor(MainApplication.getContext(), R.color.dark_theme_background)).substring(2));
+				theme.put(TEXT_COLOR_READ, "#"+Integer.toHexString(ContextCompat.getColor(MainApplication.getContext(), R.color.dark_theme_color_read)).substring(2));
 				theme.put(MENU_FONT_COLOR, "#FFFFFF");
 				theme.put(MENU_BACKGROUND_COLOR, "#222222");
 				theme.put(QUOTE_BACKGROUND_COLOR, "#000000");
@@ -83,19 +96,43 @@ public class Theme {
 				theme.put(SUBTITLE_BORDER_COLOR, "#303030");
 				//theme.put(ENTRY_LIST_BACKGROUND, "#ff202020");
 				theme.put(STYLE_THEME, String.valueOf( R.style.Theme_Dark) );
+				theme.put("toolBarColor", "#"+Integer.toHexString(ContextCompat.getColor(MainApplication.getContext(), R.color.dark_theme_color_primary)).substring(2));
 
 
 //				theme.put("themeDialog", String.valueOf( R.style.styleDarkDialog) );
 				ThemeList.put(DARK, theme);
 			}
-			
+
+			{
+				HashMap<String, String> theme = new HashMap<String, String>();
+//				theme.put(TEXT_COLOR, "#FFFFFF");
+				theme.put(TEXT_COLOR, "#"+Integer.toHexString(ContextCompat.getColor(MainApplication.getContext(), R.color.black_theme_color_unread)).substring(2));
+//				theme.put(TEXT_COLOR_BACKGROUND, "#181b1f");
+				theme.put(TEXT_COLOR_BACKGROUND, "#"+Integer.toHexString(ContextCompat.getColor(MainApplication.getContext(), R.color.black_theme_background)).substring(2));
+				theme.put(TEXT_COLOR_READ, "#"+Integer.toHexString(ContextCompat.getColor(MainApplication.getContext(), R.color.black_theme_color_read)).substring(2));
+				theme.put(MENU_FONT_COLOR, "#FFFFFF");
+				theme.put(MENU_BACKGROUND_COLOR, "#222222");
+				theme.put(QUOTE_BACKGROUND_COLOR, "#000000");
+				theme.put(QUOTE_LEFT_COLOR, "#FFA500");
+				//theme.put(BUTTON_COLOR, "#1A5A81");
+				theme.put(SUBTITLE_COLOR, "#8c8c8c");
+				theme.put(SUBTITLE_BORDER_COLOR, "#303030");
+				//theme.put(ENTRY_LIST_BACKGROUND, "#ff202020");
+				theme.put(STYLE_THEME, String.valueOf( R.style.Theme_Black) );
+				theme.put("toolBarColor", "#"+Integer.toHexString(ContextCompat.getColor(MainApplication.getContext(), R.color.black_theme_color_primary)).substring(2));
+
+
+//				theme.put("themeDialog", String.valueOf( R.style.styleDarkDialog) );
+				ThemeList.put(BLACK, theme);
+			}
+
 			for ( HashMap<String, String> theme: ThemeList.values() ) {
 				theme.put("notificationBackgroundColor", theme.get(MENU_BACKGROUND_COLOR) );
-				theme.put(TEXT_COLOR_READ, MainApplication.getContext().getString( R.string.default_read_color ));
+//				theme.put(TEXT_COLOR_READ, MainApplication.getContext().getString( R.string.default_read_color ));
 				theme.put(LINK_COLOR, MainApplication.getContext().getString( R.string.default_link_color ) );
 				theme.put(LINK_COLOR_BACKGROUND, theme.get( TEXT_COLOR_BACKGROUND ) );
 				theme.put(TEXT_COLOR_READ_BACKGROUND, theme.get( TEXT_COLOR_BACKGROUND ) );
-				theme.put("toolBarColor", MainApplication.getContext().getString( R.string.default_toolbar_color));
+//				theme.put("toolBarColor", MainApplication.getContext().getString( R.string.default_toolbar_color));
 			}
 			mTheme = PrefUtils.getString( THEME, DARK);
 		}
