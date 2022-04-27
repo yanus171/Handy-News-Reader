eb2f3c11ff8bd28ec1ee1dc6e1e2489bf708bc3f
yanus171 <workyalex@mail.ru>
2022-04-27 19:40:56 +0300

    +Date format pref

--- FlymFork/src/main/java/ru/yanus171/feedexfork/utils/StringUtils.java
+++ FlymFork/src/main/java/ru/yanus171/feedexfork/utils/StringUtils.java
@@ -20,9 +20,11 @@
 package ru.yanus171.feedexfork.utils;
 
 import android.annotation.TargetApi;
+import android.content.Context;
 import android.os.Build;
 
 import ru.yanus171.feedexfork.MainApplication;
+import ru.yanus171.feedexfork.R;
 
 import java.math.BigInteger;
 import java.security.MessageDigest;
@@ -31,6 +33,7 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
 
 @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR2)
 public class StringUtils {
@@ -48,24 +51,157 @@ public class StringUtils {
         }
     }
 
-    static public String getDateTimeString(long timestamp) {
-        String outString;
+    // -------------------------------------------------------------------------
+    public static String MilliSecIntervalToString(final long ms, boolean shortCaption, boolean trimMinutesInBigHours) {
+        String result = "";
+        long absMs = Math.abs(ms);
+        int days = (int) absMs / (24 * 60 * 60 * 1000);
+        absMs = absMs % (24 * 60 * 60 * 1000);
+        int hours = (int) absMs / (60 * 60 * 1000);
+        absMs = absMs % (60 * 60 * 1000);
+        int minutes = (int) absMs / (60 * 1000);
 
-        Date date = new Date(timestamp);
-        Calendar calTimestamp = Calendar.getInstance();
-        calTimestamp.setTimeInMillis(timestamp);
-        Calendar calCurrent = Calendar.getInstance();
-
-        if ( Math.abs( calCurrent.getTimeInMillis() - timestamp ) < SIX_HOURS ||
-             calCurrent.get(Calendar.DAY_OF_MONTH) == calTimestamp.get(Calendar.DAY_OF_MONTH)) {
-            outString = TIME_FORMAT.format(date);
-        } else {
-            outString = DATE_SHORT_FORMAT.format(date) + ' ' + TIME_FORMAT.format(date);
+        if (days > 0) {
+            result = result.trim() + DaysToString(days, shortCaption);
+        }
+        if (hours > 0 && (days < 2 || !trimMinutesInBigHours)) {
+            result = result.trim() + " " + HoursToString(hours, shortCaption);
+        }
+        if (minutes > 0 && (hours < 2 || !trimMinutesInBigHours) && days == 0) {
+            result = result.trim() + " " + MinutesToString(minutes, shortCaption);
         }
 
-        if ( calCurrent.get(Calendar.YEAR) != calTimestamp.get(Calendar.YEAR) )
-            outString = String.valueOf( calTimestamp.get(Calendar.YEAR) ) + " " + outString;
-        return outString;
+        return result.trim();
+    }
+    public static String DaysToString(int days, boolean shortCaption) {
+        final Context context = MainApplication.getContext();
+        String result;
+        if (shortCaption) {
+            result = context.getString(R.string.daysShort);
+        } else if (Locale.getDefault().getLanguage().equals("en")) {
+            result = context.getString(R.string.oneDay);
+            if (days > 1) {
+                result = context.getString(R.string.fiveDay);
+            }
+        } else {
+            int modDays = days % 10;
+            if ((modDays == 0) || ((days >= 5) && (days <= 20))) {
+                result = context.getString(R.string.fiveDay);
+            } else if (modDays == 1) {
+                result = context.getString(R.string.oneDay);
+            } else if ((modDays > 1) && (modDays <= 4)) {
+                result = context.getString(R.string.twoFourDay);
+            } else {
+                result = context.getString(R.string.fiveDay);
+            }
+        }
+        result = String.format("%d %s", days, result);
+        return result;
+    }
+    // ---------------------------------------------------------------------------------
+    private static String HoursToString(int hours, boolean shortCaption) {
+        final Context context = MainApplication.getContext();
+
+        String result;
+        if (shortCaption) {
+            result = Integer.toString(hours) + context.getString(R.string.hoursShort);
+        } else {
+            if (Locale.getDefault().getLanguage().equals("en")) {
+                result = context.getString(R.string.oneHour);
+                if (hours > 1) {
+                    result = context.getString(R.string.fiveHour);
+                }
+            } else {
+                int mod = hours % 10;
+                if ((mod == 0) || ((hours >= 5) && (hours <= 20))) {
+                    result = context.getString(R.string.fiveHour);
+                } else if (mod == 1) {
+                    result = context.getString(R.string.oneHour);
+                } else if ((mod > 1) && (mod <= 4)) {
+                    result = context.getString(R.string.twoFourHour);
+                } else {
+                    result = context.getString(R.string.fiveHour);
+                }
+            }
+            result = String.format("%d %s", hours, result);
+        }
+        return result;
+    }
+    // ---------------------------------------------------------------------------------
+    private static String MinutesToString(int minutes, boolean shortCaption) {
+        final Context context = MainApplication.getContext();
+        String result;
+        if (shortCaption) {
+            result = Integer.toString(minutes) + context.getString(R.string.minutesShort);
+        } else {
+            if (Locale.getDefault().getLanguage().equals("en")) {
+                result = context.getString(R.string.oneMinute);
+                if (minutes > 1) {
+                    result = context.getString(R.string.fiveMinute);
+                }
+            } else {
+                int mod = minutes % 10;
+                if ((mod == 0) || ((minutes >= 5) && (minutes <= 20))) {
+                    result = context.getString(R.string.fiveMinute);
+                } else if (mod == 1) {
+                    result = context.getString(R.string.oneMinute);
+                } else if ((mod > 1) && (mod <= 4)) {
+                    result = context.getString(R.string.twoFourMinute);
+                } else {
+                    result = context.getString(R.string.fiveMinute);
+                }
+            }
+
+            result = String.format("%d %s", minutes, result);
+        }
+        return result;
+    }
+
+    private static String IntervalFromNowToStringInner(long ms1, boolean shortCaption, boolean trimMinutesInBigHours) {
+        final Context context = MainApplication.getContext();
+        if (shortCaption) {
+            ms1 = Math.abs(ms1);
+        }
+        String result = MilliSecIntervalToString(ms1, shortCaption, trimMinutesInBigHours);
+        if (result.length() == 0) {
+            if (shortCaption) {
+                result = MinutesToString(0, shortCaption);
+            } else {
+                result = context.getString(R.string.rightNow);
+            }
+        } else if (!shortCaption) {
+            if (ms1 >= 1000 * 60) {
+                result = context.getString(R.string.timeIntervalInFuture) + " " + result.trim();
+            } else if (ms1 <= -1000 * 60) {
+                result = result.trim() + " " + context.getString(R.string.timeIntervalInPast);
+            }
+        }
+        return result;
+    }
+
+    static final String DEFAULT_DATETIME_FORMAT = "system";
+    static public String getDateTimeString(long timestamp) {
+        if ( PrefUtils.getString( "datetime_format", DEFAULT_DATETIME_FORMAT ).equals( "remaining" ) ) {
+            return IntervalFromNowToStringInner(  timestamp - System.currentTimeMillis(), true, true );
+        } else {
+            String outString;
+
+            Date date = new Date(timestamp);
+            Calendar calTimestamp = Calendar.getInstance();
+            calTimestamp.setTimeInMillis(timestamp);
+            Calendar calCurrent = Calendar.getInstance();
+
+            if (Math.abs(calCurrent.getTimeInMillis() - timestamp) < SIX_HOURS ||
+                calCurrent.get(Calendar.DAY_OF_MONTH) == calTimestamp.get(Calendar.DAY_OF_MONTH)) {
+                outString = TIME_FORMAT.format(date);
+            } else {
+                outString = DATE_SHORT_FORMAT.format(date) + ' ' + TIME_FORMAT.format(date);
+            }
+
+            if (calCurrent.get(Calendar.YEAR) != calTimestamp.get(Calendar.YEAR))
+                outString = String.valueOf(calTimestamp.get(Calendar.YEAR)) + " " + outString;
+            return outString;
+        }
     }
 
     public static String getMd5(String input) {
