fddc8f6b293bbb27900ff96c620fb48bc89086ce
yanus171 <workyalex@mail.ru>
2023-12-10 18:07:43 +0300

    parseDate synchronized, v0.19.13

--- FlymFork/src/main/java/ru/yanus171/feedexfork/parser/RssAtomParser.java
+++ FlymFork/src/main/java/ru/yanus171/feedexfork/parser/RssAtomParser.java
@@ -601,34 +601,38 @@ public class RssAtomParser extends DefaultHandler {
     }
 
     public static Date parseDate(String dateStr, long now) {
-        Dog.d( "parseDate " + dateStr );
-        long dateBorder = 30 * 365 * 24 * 60 * 60 * 1000L; // twenty years ago
-        dateStr = improveDateString(dateStr);
-        Date result = new Date();
-        for (DateFormat format : DATE_FORMATS ) {
-            try {
-                result = format.parse(dateStr);
-                if ( now == 0 )
-                    return result;
-                if ( now - result.getTime() > dateBorder )
-                    continue;
-                if ( Math.abs( result.getTime() -  now ) < dateBorder )
-                    return (result.getTime() > now ? new Date(now) : result);
-            } catch (ParseException ignored) {
+        synchronized ( DATE_FORMATS ) {
+            Dog.d("parseDate " + dateStr);
+            long dateBorder = 30 * 365 * 24 * 60 * 60 * 1000L; // twenty years ago
+            dateStr = improveDateString(dateStr);
+            Date result = new Date();
+            for (DateFormat format : DATE_FORMATS) {
+                try {
+                    result = format.parse(dateStr);
+                    if (now == 0)
+                        return result;
+                    if (now - result.getTime() > dateBorder)
+                        continue;
+                    if (Math.abs(result.getTime() - now) < dateBorder) {
+                        if (result.getTime() > now)
+                            result = new Date(now);
+                        return result;
+                    }
+                } catch (ParseException ignored) {
 
-            } // just do nothing
+                } // just do nothing
 
+            }
+            if (now - result.getTime() > dateBorder) {
+                Calendar today = Calendar.getInstance();
+                Calendar cal = Calendar.getInstance();
+                cal.setTimeInMillis(result.getTime());
+                cal.set(today.get(Calendar.YEAR), today.get(Calendar.MONTH), today.get(Calendar.DATE));
+                if (cal.after(today))
+                    cal.add(Calendar.DATE, -1);
+                return new Date(cal.getTimeInMillis());
+            }
         }
-        if (  now - result.getTime() > dateBorder ) {
-            Calendar today = Calendar.getInstance();
-            Calendar cal = Calendar.getInstance();
-            cal.setTimeInMillis( result.getTime() );
-            cal.set( today.get( Calendar.YEAR ), today.get( Calendar.MONTH ), today.get( Calendar.DATE ) );
-            if ( cal.after( today ) )
-                cal.add( Calendar.DATE, -1 );
-            return new Date( cal.getTimeInMillis() );
-        }
-
         return null;
     }
 
